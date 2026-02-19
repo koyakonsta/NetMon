193641e425005e53364194cb3969eb4fc815944e
Regaus <42419910+Regaus@users.noreply.github.com>
2026-02-19 23:39:42 +0000

    Update application and package name to "Network Monitor"

--- code/nativescript.config.ts
+++ code/nativescript.config.ts
@@ -1,7 +1,7 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-  id: 'org.nativescript.code',
+  id: 'org.dcu.network_monitor',
   appPath: 'app',
   appResourcesPath: 'App_Resources',
   android: {
